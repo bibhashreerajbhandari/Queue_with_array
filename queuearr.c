9b8a74c67ba50bc62da9fd46a6cbb23a232bfd03
bibhashreerajbhandari <bibhashree.ad24@bmsce.ac.in>
2025-11-03 20:09:31 +0530

    Update queuearr.c

--- queuearr.c
+++ queuearr.c
@@ -1,64 +1,89 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 #define MAX 100
+
 int queue_arr[MAX];
 int rear = -1;
 int front = -1;
+
 void insert();
 void delete();
 void display();
+
 void main()
 {
-int choice;
-while (1)
+    int choice;
+
+    while (1)
+    {
+        printf("\n1. Insert\n2. Delete\n3. Display\n4. Exit\n");
+        printf("Enter choice: ");
+        scanf("%d", &choice);
+
+        switch (choice)
+        {
+        case 1:
+            insert();
+            break;
+        case 2:
+            delete();
+            break;
+        case 3:
+            display();
+            break;
+        case 4:
+            exit(0);
+        default:
+            printf("Invalid input\n");
+        }
+    }
+}
+
+
+void insert()
 {
-printf("1.insert\n2.delete\n3.display\n4.exit");
-printf("Enter choice: ");
-scanf('%d',&choice);
-switch(choice){
-case 1: insert();
-break;
-case 2: delete();
-break;
-case 3: display();
-break;
-case 4: exit(1);
-default:
-printf("error input");
+    int add_item;
+    if (rear == MAX - 1)
+    {
+        printf("Queue Overflow\n");
+    }
+    else
+    {
+        if (front == -1)
+            front = 0;
+        printf("Enter value to add to queue: ");
+        scanf("%d", &add_item);
+        rear = rear + 1;
+        queue_arr[rear] = add_item;
+    }
 }
-}
-display();
+
+void delete()
 {
-int i;
-if (front == -1){
-printf("Queue is empty");
+    if (front == -1 || front > rear)
+    {
+        printf("Queue Underflow\n");
+    }
+    else
+    {
+        printf("Deleted: %d\n", queue_arr[front]);
+        front = front + 1;
+    }
 }
-else{
-print("queue is: ");
-for (i=front;i<=rear;i++)
-printf("%d",queue_arr[i]);
-printf("\n");
-}
-}
-insert();{
-int add_item;
-if(rear==MAX-1)
-printf("queue is full. overflow");
-else{
-printf("enter value to add to queue:");
-scanf("%d",&add_item);
-rear=rear+1;
-queue_arr[rear]=add_item;
-}
-}
-delete();{
-if(front==-1 || front>rear)
+
+void display()
 {
-printf("queue underflow");
-return;
-}
-else{
-printf("deleted= %d",queue_arr[front]);
-front=front+1;
-}
-}
+    int i;
+    if (front == -1 || front > rear)
+    {
+        printf("Queue is empty\n");
+    }
+    else
+    {
+        printf("Queue is: ");
+        for (i = front; i <= rear; i++)
+            printf("%d ", queue_arr[i]);
+        printf("\n");
+    }
 }
